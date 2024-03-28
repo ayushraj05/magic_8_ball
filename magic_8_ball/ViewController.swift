030f0d83ca2a6154dca9afa2c184c3e8b24dc80c
ayushraj05 <ayushrajpal2021@gmail.com>
2024-03-28 23:24:55 +0530

    fixed the text issue

--- magic_8_ball/ViewController.swift
+++ magic_8_ball/ViewController.swift
@@ -14,6 +14,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var Ball_image_view: UIImageView!
+    
+    let BallArray = [ #imageLiteral(resourceName: "ball_3"), #imageLiteral(resourceName: "ball_4") , #imageLiteral(resourceName: "ball_1") , #imageLiteral(resourceName: "ball_5") , #imageLiteral(resourceName: "ball_4") ]
+    
+    @IBAction func Ask_button(_ sender: UIButton) {
+        print("Button is pressed :)")
+        let randomInterger = Int.random(in : 0..<5)
+        Ball_image_view.image = BallArray[randomInterger]
+        
+    }
+    
 }
 
